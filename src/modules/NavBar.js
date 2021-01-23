38f3cad3a51493f88baa3741cc6c851c1a62475a
camimgh <josephinecmejia@gmail.com>
2021-01-23 15:50:06 -0500

    Starting to form navbar.

--- src/modules/NavBar.js
+++ src/modules/NavBar.js
@@ -1,2 +1,15 @@
+import React, { Component } from "react";
 import logo from '../logo.png'
 import './NavBar.css'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="NavBar-items">
+                <h1 className="NavBar-title">cami m.</h1>
+                <p className="NavBar-timeline">timeline</p>
+            </nav>
+        )
+    }
+}
+export default NavBar
