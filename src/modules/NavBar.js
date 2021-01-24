00d19caf5387f2a1c3708ecc1e58b5726956ed73
camimgh <josephinecmejia@gmail.com>
2021-01-23 21:01:04 -0500

    Imported react-bootstrap and edited navbar.

--- src/modules/NavBar.js
+++ src/modules/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Nav } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../logo.png'
 import './NavBar.css'
@@ -6,9 +7,16 @@ import './NavBar.css'
 class NavBar extends Component {
     render() {
         return (
-            <Navbar bg="dark" variant="dark">
-               <Navbar.Brand href="#home">cami m.</Navbar.Brand> 
-            </Navbar>
+                <Navbar className="NavBar-box" sticky="top" variant="light">
+                   <Navbar.Brand href="#home">cami m.</Navbar.Brand> 
+                   <Nav>
+                       <Nav.Link className="NavBar-item" href="#about">about</Nav.Link>
+                       <Nav.Link href="#projects">projects</Nav.Link>
+                       <Nav.Link href="#experience">experience</Nav.Link>
+                       <Nav.Link href="#timeline">timeline</Nav.Link>
+                       <Nav.Link href="#writing">writing</Nav.Link>
+                   </Nav>
+                </Navbar>
         )
     }
 }
