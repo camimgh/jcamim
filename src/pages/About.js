a8cfd06212636a727ef5569a72b6d8349bc8c2f9
camimgh <josephinecmejia@gmail.com>
2021-01-24 18:15:26 -0500

    made text and image responsive.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./About.css";
 import logo from '../logo-cropped.png';
-import "./About.scss";
-import { Picture } from 'react-responsive-picture';
+import small from '../apple-touch-icon.png';
+
 class About extends Component {
     render() {
         return(
             <div className="intro-box">
-                <Picture className="logo" src={logo} alt="logo" />
+                <img className="logo" src={logo} alt="logo" />
                 <div className="vl">
                     <h1 className="header-text">Hi! My name is Cami Mejia and I'm a sophomore at MIT studying computer science and media studies. Some of my interests include gaming, project management, social media, and writing. Feel free to look around.</h1>
                 </div>
