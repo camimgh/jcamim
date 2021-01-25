af85445c7ab62b1929fcaa175c2bd089bf8e8ca8
camimgh <josephinecmejia@gmail.com>
2021-01-24 19:18:14 -0500

    Added fontawesome icon and linked to about header. Smooth scrolling.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./About.css";
 import logo from '../logo-cropped.png';
-import small from '../apple-touch-icon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 class About extends Component {
     render() {
         return(
-            <div className="intro-box">
-                <img className="logo" src={logo} alt="logo" />
-                <div className="vl">
-                    <h1 className="header-text">Hi! My name is Cami Mejia and I'm a sophomore at MIT studying computer science and media studies. Some of my interests include gaming, project management, social media, and writing. Feel free to look around.</h1>
+            <div>
+                <div className="intro-box">
+                    <img className="logo" src={logo} alt="logo" />
+                    <div className="vl">
+                        <h1 className="header-text">Hi! My name is Cami Mejia and I'm a sophomore at MIT studying computer science and media studies. Some of my interests include gaming, project management, social media, and writing. Feel free to look around.</h1>
+                    </div>
+                </div>
+                <a href="#about"><FontAwesomeIcon size="lg" color="grey" className="arrow" icon={faChevronDown} /></a>
+                <div className="about-me">
+                    <h1 id="about">About Me</h1>
                 </div>
             </div>
         )
