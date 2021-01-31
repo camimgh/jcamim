7512c130461ab35526e14734b169ef57c7e0233f
camimgh <raytran@mit.edu>
2021-01-31 18:48:42 -0500

    test

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 class Timeline extends Component {
     render() {
         return(
-            <p>hi</p>
+            <p>hi hello</p>
         )
     }
 }
