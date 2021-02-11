a8ba90abef0a8cfd090b5f2f2e569081c021b678
camimgh <josephinecmejia@gmail.com>
2021-02-11 00:36:09 -0500

    edited dive page

--- src/pages/pages_projects/Dive.js
+++ src/pages/pages_projects/Dive.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import Divemd from "./Dive.md";
 import './Dive.css'
 import gfm from 'remark-gfm';
-
+import Card from 'react-bootstrap/Card'
 /*
 export default function Dive() {
     return (
@@ -32,7 +32,16 @@ class Dive extends Component {
         return (
             <div id="dive">
                 <h1>Dive</h1>
-                <ReactMarkdown plugins={[gfm]} source={markdown} />
+                <Card body>
+                <p>
+                        Dive is a mobile app built with *Typescript* and *React* that connects people to others inside or outside of their culture to encourage diversity, personal connection, and friendship.
+                        My team of 5 worked on the front-end of Dive for Microsoft's [New Technologist program](https://newtechnologists.com).
+                </p>
+                <p align="center"><iframe width="300" height="315" 
+                    src="https://www.youtube.com/embed/gfzJSjN6rV8" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+                    <ReactMarkdown plugins={[gfm]} source={markdown} />
+                </Card>
             </div>
         )
     }
--- src/pages/pages_projects/Dive.md
+++ src/pages/pages_projects/Dive.md
@@ -1,2 +1,10 @@
-## Intro
-Dive is.
+Dive is a mobile app built with *Typescript* and *React* that connects people to others inside or outside of their culture to encourage diversity, personal connection, and friendship.
+
+My team of 5 worked on the front-end of Dive for Microsoft's [New Technologist program](https://newtechnologists.com).
+
+## Planning
+Before we could begin creating our project, we had to decide upon a prompt.
+
+We chose to build the front-end of an app focused on **removing barriers to expressing and sharing ideas.**
+
+
